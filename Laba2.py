2b01ad2ba65c47e7e9cdc02e5dd6ac30be5ea0be
Krikiboli <mulderf0f@gmail.com>
2020-05-10 16:18:00 +1000

    first commit

---
+++ Laba2.py
@@ -0,0 +1,35 @@
+dic = {}
+
+with open('conf.txt', 'r') as file:
+    for line in file:
+        if line[0] != '#' and line[0] != ';' and line[0] != '\n':
+            if '\n' in line:
+                line = line[:-1]
+
+            line = line.split(' ', 1)
+
+            if len(line) > 1:
+                key = line[0]
+                dic[key] = line[1]
+            else:
+                key = line[0]
+
+                dic[key] = "no value"
+flag = True
+while flag:
+
+    num = input("Enter parameter (get param ....): ")
+    slovo = num.split()
+    counter = 0
+    for key in dic:
+        if slovo[2] == key:
+            print(key, '= ', dic[key])
+            counter += 1
+    if counter == 0:
+        print('no parameter')
+    command = input("Continue? Yes or No: ")
+    if command.lower() == 'yes':
+        flag = True
+    elif command.lower() == "no":
+        flag = False
+
